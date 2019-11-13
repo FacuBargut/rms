7c0828b599f3d319baec5f13b8272ec4d80c3a7e
Facu <facubargut@gmail.com>
2019-11-13 03:04:56 -0300

    Avanzando

---
+++ components/header/header.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-sm bg-dark header">
+    <a class="navbar-brand text-white logo" href="#">RMS-Rosario Music Shop</a>
+
+    <form class="form-inline m-auto searchForm">
+        <input class="form-control"
+               type="search"
+               placeholder="Search"
+               >
+        <button class="btn btn-info" type="submit"><i class="fas fa-search"></i></button>
+    </form>
+    <ul class="navbar-nav ml-auto userOptions">
+        <li class="nav-item ">
+            <a class="nav-link text-white" href="#">
+                <i class="fas fa-user"></i>
+            </a>       
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link text-white" href="#">
+                <i class="fas fa-shopping-cart"></i>
+            </a>       
+        </li>
+    </ul>
+</nav>
--- index.php
+++ index.php
@@ -5,11 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/css/all.css" rel="stylesheet">
+
+    <!-- Estilos header -->
+    <link rel="stylesheet" href="components/header/header.css">
+
     <title>Rosario Music Shop</title>
 </head>
 <body>
 
-<h1>Hola!</h1>
+<!-- header -->
+
+    <?php include "components/header/header.php"; ?>
+
+
+
+
     
 </body>
 </html>
+
+
